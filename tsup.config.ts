f8013c396b575ef435f48ecd6bbeb7d68fc5b323
Simone Lusenti <lusenti.s@gmail.com>
2024-01-23 11:29:57 +0100

    remove bundlesize
    Signed-off-by: Simone Lusenti <lusenti.s@gmail.com>

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  dts: true,
   clean: true,
-  sourcemap: false,
-  format: ["esm"],
-  minify: true,
-  treeshake: true,
-  metafile: true,
+  dts: true,
+  entry: ["src/index.ts"],
   external: ["react", "react-dom"],
+  format: ["esm"],
+  metafile: false,
+  minify: true,
+  sourcemap: false,
+  treeshake: true,
 });
